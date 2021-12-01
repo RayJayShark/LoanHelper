c230e70e73f5bfad2a3f8523fd11fcaf0e35ff2e
Joshua Ray <superdog1695@yahoo.com>
2021-12-01 17:52:41 -0500

    Moved PaymentCalculator page to AntBlazor components. Moved loan functionality to Loan.cs class.

---
+++ LoanHelper/Models/Loan.cs
@@ -0,0 +1,53 @@
+using System;
+
+namespace LoanHelper.Models
+{
+    public class Loan
+    {
+        public double PrincipalValue { get; set; }
+        public double InterestRate { get; set; }
+        public InterestFrequency Frequency { get; set; }
+        public int NumberOfPeriods { get; set; }
+        public double PaymentAmount { get; set; }
+
+        public enum InterestFrequency
+        {
+            Annually,
+            Monthly
+        }
+        
+        /// <summary>
+        /// Calculates new InterestRate based on new InterestFrequency
+        /// Example: Changing from Annually to Monthly divides the interest rate by 12
+        /// </summary>
+        public void ChangeInterestType()
+        {
+            // No calculation needed
+            if (InterestRate == 0) return;
+            
+            // Changed to annually
+            if (Frequency == InterestFrequency.Annually)
+            {
+                InterestRate *= 12;
+            }
+            // Changed to monthly
+            else
+            {
+                InterestRate /= 12d;
+            }
+        }
+        
+        /// <summary>
+        /// Calculates amount of payments when all other info is given
+        /// </summary>
+        public void CalculatePayment()
+        {
+            // Check for valid data. Just returns for now.
+            if (PrincipalValue <= 0 || NumberOfPeriods <= 0 || InterestRate < 0) return;
+            
+            // r(PV)/1-(1+r)^-n
+            var percentRate = Frequency == InterestFrequency.Annually ? (InterestRate / 100d) / 12d : InterestRate / 100d;
+            PaymentAmount = (percentRate * PrincipalValue) / (1 - Math.Pow(1 + percentRate, NumberOfPeriods * -1));
+        }
+    }
+}
--- LoanHelper/Pages/Tools/PaymentCalculator.razor.cs
+++ LoanHelper/Pages/Tools/PaymentCalculator.razor.cs
@@ -1,39 +1,11 @@
 using System;
+using LoanHelper.Models;
 using Microsoft.AspNetCore.Components;
 
 namespace LoanHelper.Pages.Tools
 {
     public partial class PaymentCalculator : ComponentBase
     {
-        private double _loanValue;
-        private double _interestRate;
-        private int _numberOfPayments;
-        private double _paymentAmount;
-
-        private int _interestType = 0;
-
-        private void ChangeInterestType()
-        {
-            // No calculation needed
-            if (_interestRate == 0) return;
-            
-            // Changed to annual
-            if (_interestType == 0)
-            {
-                _interestRate *= 12;
-            }
-            // Changed to monthly
-            else
-            {
-                _interestRate /= 12d;
-            }
-        }
-        
-        private void CalculatePayment()
-        {
-            // r(PV)/1-(1+r)^-n
-            var percentRate = _interestType == 0 ? (_interestRate / 100d) / 12d : _interestRate / 100d;
-            _paymentAmount = (percentRate * _loanValue) / (1 - Math.Pow(1 + percentRate, _numberOfPayments * -1));
-        }
+        private readonly Loan _loan = new Loan();
     }
 }
